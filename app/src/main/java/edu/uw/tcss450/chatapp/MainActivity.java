145c1a3447289f016ed2229548ff4ffa9d10a77f
Seungku Kim <tmdrbtl1@uw.edu>
2021-04-28 13:04:28 -0700

    Implemented Registaration, Sigin in
    I had little trouble on themes, and styles on the night view. if anybody know how to fix it  they should otherwise I think it is better to just delete the theme and implement it laster.

--- app/src/main/java/edu/uw/tcss450/chatapp/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/chatapp/MainActivity.java
@@ -1,8 +1,12 @@
 package edu.uw.tcss450.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+
+import edu.uw.tcss450.chatapp.model.UserInfoViewModel;
+import edu.uw.tcss450.lab3_authentication.MainActivityArgs;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,16 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    //Aman testing 2 (pull request)
-    //Aman testing 1
-    //testing1
-    //Taehong Kim Testing pushing
-    // Seungku Kim Testing pushing
-    // Sample pull request comment
-    // Seungku Kim Testing Again with Branch
-    // Taehong Kim testing again with branch
-    // taehong kim branch testing
 
+        MainActivityArgs args = MainActivityArgs.fromBundle(getIntent().getExtras());
+
+        new ViewModelProvider(this,
+                new UserInfoViewModel.UserInfoViewModelFactory(args.getEmail(), args.getJwt())
+        ).get(UserInfoViewModel.class);
+
+    }
 
 }
